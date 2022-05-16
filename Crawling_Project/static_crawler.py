3a07c68c6935f6a9599b32c34a4bae5e429cd011
hoho <kumass2021@gmail.com>
2022-05-16 14:14:23 +0900

    working regex

--- Crawling_Project/static_crawler.py
+++ Crawling_Project/static_crawler.py
@@ -1,32 +1,40 @@
 from bs4 import BeautifulSoup
-import urllib.request
 import pandas as pd
+import requests
+import re
 
+
+# 정규표현식 사용 
+def regularize(str1):
+    return re.sub('[^0-9a-zA-Zㄱ-힗\r]','',str(str1))
 
 # 크롤링 수행
 def crawl(result):
     for page in range(1, 138):
         # URL 지정
-        cs_url = 'https://www.gachon.ac.kr/cs/5905/subview.do?%d' % page
+        cs_url = 'https://www.gachon.ac.kr/cs/5905/subview.do?page=%d' % page
         print(cs_url)
 
-        # urllib, BeautifulSoup 객체 호출
-        html = urllib.request.urlopen(cs_url)
+        # requests, BeautifulSoup 객체 호출
+        # html = urllib.request.urlopen(cs_url)
+        html = requests.get(cs_url).text
         soup = BeautifulSoup(html, 'html.parser')
 
         # tbody 태그로 검색
         tag_tbody = soup.find('tbody')
 
         # tbody 태그 하위 tr 태그를 (첫번째 요소 제외하고) 모두 찾음
-        for content in tag_tbody.find_all('tr')[1:]:
+        for parent_content in tag_tbody.find_all('tr')[1:]:
+            # tr 태그 하위 td 태그를 모두 찾음
+            content = parent_content.find_all('td')
             # 게시물의 각 정보를 string 형태로 저장
-            content_id = content[0].string
-            content_subject = content[1].string.replace('N', '')    # 필요없는 텍스트 제거
-            content_writer = content[2].string
-            content_date = content[3].string
-            content_view = content[4].string
-            content_comment = content[5].string
-            content_file_num = content[6].string
+            content_id = regularize(content[0].string)
+            content_subject = regularize(content[1].text).rstrip('N')    # 필요없는 텍스트 제거
+            content_writer = regularize(content[2].string)
+            content_date = regularize(content[3].string)
+            content_view = regularize(content[4].string)
+            content_comment = regularize(content[5].string)
+            content_file_num = regularize(content[6].string)
             # result 배열에 저장
             result.append([content_id]+[content_subject]+[content_writer]+[content_date]
                         +[content_view]+[content_comment]+[content_file_num])
@@ -34,13 +42,14 @@ def crawl(result):
 
 if __name__=='__main__':
     result = []
-    print('Gachon CS crawling >>>>>>>>>>>>>>>>>>>>>>>>')
+    print('Gachon CS Notice crawling >>>>>>>>>>>>>>>>>>>>>>>>')
     crawl(result)
 
     # 해당 attribute를 가진 데이터프레임 생성
     tbl = pd.DataFrame(result, columns={'id', 'subject', 'writer', 'date', 'view', 'comment', 'file'})
 
     # DataFrame to CSV file
-    tbl.to_csv('./Gachon_CS_Notice.csv', encoding='cp949', mode='w', index=True)
+    tbl.columns
+    tbl.to_csv('./Gachon_CS_Notice.csv', encoding='utf-8', mode='w', index=False)
 
     del result[:]
