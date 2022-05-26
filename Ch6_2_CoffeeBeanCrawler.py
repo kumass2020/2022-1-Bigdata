533f56a7c544c9691c915a5f58a73f6bcf3546be
hoho <kumass2021@gmail.com>
2022-05-27 05:19:59 +0900

    first solution

--- Ch6_2_CoffeeBeanCrawler.py
+++ Ch6_2_CoffeeBeanCrawler.py
@@ -4,31 +4,37 @@ import pandas as pd
 import datetime
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 #[CODE 1]
 def CoffeeBean_store(result):
     CoffeeBean_URL = "https://www.coffeebeankorea.com/store/store.asp"
-    wd = webdriver.Chrome('./WebDriver/chromedriver')
+    wd = webdriver.Chrome('./chromedriver.exe')
              
-    for i in range(1, 370):  #매장 수 만큼 반복
-        wd.get(CoffeeBean_URL)
-        time.sleep(1)  #웹페이지 연결할 동안 1초 대기
-        try:
-            wd.execute_script("storePop2(%d)" %i)
-            time.sleep(1) #스크립트 실행 할 동안 1초 대기
-            html = wd.page_source
-            soupCB = BeautifulSoup(html, 'html.parser')
-            store_name_h2 = soupCB.select("div.store_txt > h2")
-            store_name = store_name_h2[0].string
-            print(store_name)  #매장 이름 출력하기
-            store_info = soupCB.select("div.store_txt > table.store_table > tbody > tr > td")
-            store_address_list = list(store_info[2])
-            store_address = store_address_list[0]
-            store_phone = store_info[3].string
-            result.append([store_name]+[store_address]+[store_phone])
-        except:
-            continue 
+    wd.get(CoffeeBean_URL)
+    time.sleep(1)
+    
+    element = wd.find_element_by_id('region_srh')
+    element.click()
+    time.sleep(1)
+    
+    element = wd.find_element_by_id('localTitle')
+    element.click()
+    time.sleep(1)
+    
+    element = wd.find_element(By.XPATH, '//*[@id="storeLocal"]/li[2]/a')
+    element.click()
+    time.sleep(1)
+
+    element = wd.find_element_by_id('localTitle2')
+    element.click()
+    time.sleep(1)
+    
+    element = wd.find_element(By.XPATH, '//*[@id="storeLocal2"]/li[9]/a')
+    element.click()
+    time.sleep(1)
+    
     return
 
 #[CODE 0]
