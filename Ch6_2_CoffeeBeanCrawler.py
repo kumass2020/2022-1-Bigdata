2e773f102cdce4a32d02a72c2a4b4601b5870adb
hoho <kumass2021@gmail.com>
2022-05-27 06:46:26 +0900

    sol 2

--- Ch6_2_CoffeeBeanCrawler.py
+++ Ch6_2_CoffeeBeanCrawler.py
@@ -19,21 +19,58 @@ def CoffeeBean_store(result):
     element.click()
     time.sleep(1)
     
-    element = wd.find_element_by_id('localTitle')
+    element = wd.find_element(By.ID, 'localTitle')
     element.click()
     time.sleep(1)
     
+    # 경기 선택
     element = wd.find_element(By.XPATH, '//*[@id="storeLocal"]/li[2]/a')
     element.click()
     time.sleep(1)
 
+    element = wd.find_element(By.ID, 'localTitle2')
+    element.click()
+    time.sleep(1)
+    
+    # 성남시 분당구 선택
+    element = wd.find_element(By.XPATH, '//*[@id="storeLocal2"]/li[9]/a')
+    element.click()
+    time.sleep(1)
+
+    ############################################################################
+    
+    # 서울 찾기 시작
+    # dropdown menu를 펼침
+    element = wd.find_element(By.ID, 'localTitle')
+    element.click()
+    time.sleep(1)
+    
+    element = wd.find_element(By.ID, 'storeLocal')
+    elements = element.find_elements(By.TAG_NAME, 'li')     # 지역명이 나와있는 리스트들을 찾고
+    for element in elements:        
+        if element.text == '경기':  # 텍스트에 경기를 갖고 있는 요소를 찾아냅니다.
+            break
+    
+    element.find_element(By.TAG_NAME, 'a')  # 해당 요소 하위 태그인 a 태그를 찾아내 클릭
+    element.click()
+    
+    time.sleep(1)
+    
+    # 성남시 분당구 찾기 시작 (위와 동일한 방식입니다.)
+    # dropdown menu를 펼침
     element = wd.find_element_by_id('localTitle2')
     element.click()
     time.sleep(1)
     
-    element = wd.find_element(By.XPATH, '//*[@id="storeLocal2"]/li[9]/a')
+    element = wd.find_element(By.ID, 'storeLocal2')
+    elements = element.find_elements(By.TAG_NAME, 'li')
+    for element in elements:
+        if element.text == '성남시 분당구':
+            break
+    
+    element.find_element(By.TAG_NAME, 'a')
     element.click()
-    time.sleep(1)
+    
     
     return
 
